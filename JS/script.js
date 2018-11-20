486390411bb5da0c8f95a6871360a8486a9f11f5
ledentsov <r.ledentsov@gmail.com>
2018-11-20 23:03:25 +0500

    1

--- JS/script.js
+++ JS/script.js
@@ -12,9 +12,6 @@ function task1function(){
                     t=true;
                     j++;
                 }
-                
-            
-            
         }
         if (t==true){
             task1value= task1value+', '+i;
@@ -22,4 +19,30 @@ function task1function(){
         i++; 
     }
     document.getElementById("task1Value").innerHTML = task1value;
+}
+
+function task2function(){
+    let i=0;
+    do{
+        if(i==0){
+            console.log(i + " Это ноль"); 
+        } else if(i%2==0){ 
+            console.log(i + " Четное"); 
+        } else console.log(i + " Нечетное");
+        i++;
+    }while(i<11)
+}
+function task3function(){
+    for(let i=0; i<10;console.log(i++));
+}
+function task4function(){
+    let str="";
+    for(let i=1; i<21;i++){
+         for(let j=0; j<i;j++){
+            str=str+"x";
+        }
+            console.log(str);
+            str="";
+        }
+    
 }
